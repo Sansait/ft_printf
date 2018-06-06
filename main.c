358409fda2106dcaea1b60c1554f69a57b8193a7
jlehideu <jlehideu@e2r2p18.42.fr>
2018-06-06 15:53:05 +0200

    test

--- main.c
+++ main.c
@@ -6,28 +6,28 @@
 /*   By: sklepper <sklepper@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/05/31 12:53:50 by sklepper          #+#    #+#             */
-/*   Updated: 2018/06/06 15:39:15 by sklepper         ###   ########.fr       */
+/*   Updated: 2018/06/06 15:51:02 by jlehideu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 
 #include "ft_printf.h"
+#include <stdio.h>
 
 int	 ft_printf(const char *str, ...)
 {
 	va_list		pointerlst;
 	char 		*ptr;
 	int			i;
-	t_data		data;
 
 	va_start(pointerlst, str);
 	i = 0;
 	while ((ptr = ft_strchr(str, '%')) != NULL)
 	{
-		ft_printuntil(str, ptr);
+		i++;
+		ft_printuntil(str, ptr) + 1;
 		ptr += 1;
-		while ((i = path(ptr, pointerlst, &data)) > 0)
-			ptr += i;
+		conversion(ptr, pointerlst, i);
 		str = ptr + 1;
 	}
 	ft_printuntil(str, ptr);
@@ -35,10 +35,17 @@ int	 ft_printf(const char *str, ...)
 	return (0);
 }
 
-
-
-int main(void)
+int main(int ac, char **av)
 {
-	ft_printf("salut les zickos %#.12d %i %d %s ca va ", 1, 2, 3, "123");
+	//ft_printf("salut les zickos %d %i %d %s ca va ", 1, 2, 3, "123");
+	int nb;
+
+	if (ac < 2)
+	{
+		printf("veuillew entrer un parametre");
+		return (0);
+	}
+	nb = ft_atoi(av[1]);
+	printf("%s\n", to_base(nb, av[2]));
 	return (0);
 }
