e25dfb86755d66c8a8c11b6f004c54343e10df70
Samuel KLEPPER <sklepper@e1r11p3.42.fr>
2018-06-07 17:16:08 +0200

    init struct

--- main.c
+++ main.c
@@ -6,13 +6,27 @@
 /*   By: sklepper <sklepper@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/05/31 12:53:50 by sklepper          #+#    #+#             */
-/*   Updated: 2018/06/07 12:05:23 by jlehideu         ###   ########.fr       */
+/*   Updated: 2018/06/07 15:35:19 by sklepper         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 
 #include "ft_printf.h"
 #include <stdio.h>
+
+void	init_struct(t_data *data)
+{
+	int i;
+
+	data->precision = 0;
+	data->width = 0;
+	i = -1;
+	while (++i <= 4)
+		data->flags[i] = 0;
+	i = -1;
+	while (++i <= 3)
+		data->length[i] = 0;
+}
 
 int	 ft_printf(const char *str, ...)
 {
@@ -22,9 +36,9 @@ int	 ft_printf(const char *str, ...)
 	t_data		data;
 
 	va_start(pointerlst, str);
-	i = 0;
 	while ((ptr = ft_strchr(str, '%')) != NULL)
 	{
+		init_struct(&data);
 		ft_printuntil(str, ptr);
 		ptr += 1;
 		while ((i = path(ptr, pointerlst, &data)) > 0)
@@ -37,20 +51,16 @@ int	 ft_printf(const char *str, ...)
 }
 
 
-int main(int ac, char **av)
+int main(void)
 {
 	int				nb;
-	unsigned int	u_nb;
+	wchar_t *str;
 
-	if (ac < 2)
-	{
-		printf("veuillez entrer un parametre");
-		return (0);
-	}
-	nb = ft_atoi(av[1]);
-	u_nb = int_to_u();
-	printf("put_u_nbr ->");
-	ft_put_u_nbr(nb);
-	printf("\nprintf ->%u\n", nb);
+	str = malloc(sizeof(wchar_t) * 10);
+
+	str[0] = 'f';
+	str[1] = 'u';
+	str[2] = '\0';
+	ft_printf("salut %300i", 230);
 	return (0);
 }
